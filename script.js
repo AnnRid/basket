49596de01f133b8bf209d02c2fc3055e91801d2f
Nina <ventio-r@rambler.ru>
2017-08-01 20:47:04 +0300

    add method fillListGoods

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
 var basket =(function(){
 
-  var list = [];
+  var listGoods = [];
   var total =0;
   var addOrder = document.querySelector('.container-basket');
   addOrder.innerHTML = '';
 
   return { 
     addGoods: function(product) {
-      list.push(product);
+      listGoods.push(product);
     },
     countSum: function() {
-      for(var i=0;i<list.length;i++) {
-        total += list[i].amount;
+      for(var i=0;i<listGoods.length;i++) {
+        total += listGoods[i].price;
       }
       return total;
     },
-    addList: function() {
-      return list.forEach(function(elem) {
-        addOrder.innerHTML +=
-        '<td>' + elem.name + '</td>' +
-        '<td>' + elem.code + '</td>' + 
-        '<td>' + elem.quantity + '</td>' + 
-        '<td>' + elem.amount + '</td>'  
-      })
-    }
+    fillListGoods: function() {
+      var containerBasket = document.querySelector('.container-basket');
+
+		var arrTd = listGoods.map (function(elem) {
+		var newTr = document.createElement('tr');
+
+		var name = document.createElement('td');
+		name.innerHTML = elem.name;
+		newTr.appendChild(name);
+
+		var articul = document.createElement('td');
+		articul.innerHTML = elem.articul;
+		newTr.appendChild(articul);
+
+		var quantity = document.createElement('td');
+		quantity.innerHTML = elem.quantity;
+		newTr.appendChild(quantity);
+
+		var price = document.createElement('td');
+		price.innerHTML = elem.price;
+		newTr.appendChild(price);
+
+		return newTr;
+		})
+		return arrTd.forEach(function(item) {
+		containerBasket.appendChild(item);
+		})
+	}
 
   }
 }());
+
+
 
